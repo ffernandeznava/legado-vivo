a81983f3311e741e0f876a3e71cab5d04140762f
ffernandez <ffernandez@ucol.mx>
2026-02-07 14:11:55 -0600

    Config: moving base path to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/legado-vivo-artisano-d7f00cb8/'
   server: {
     host: "::",
     port: 8080,
